9759eeb1ac920295ecf523ae084846e1e9d2dd3b
Tanzeem <tanzeemafsar74@gmail.com>
2026-08-01 17:57:08 +0600

    added more setup for project

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import config from "./config";
+import cookieParser from "cookie-parser";
+
+const app: Application = express();
+
+app.use(cors({
+    origin: config.app_url,
+    credentials: true
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.get('/', async (req: Request, res: Response) => {
+    res.send('Welcome to the RentNest API');
+})
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import app from "./app";
+import { prisma } from "./lib/prisma";
+import config from "./config";
+
+const PORT = config.port;
+async function main() {
+    try {
+        await prisma.$connect();
+        console.log('db connected');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+    } catch (error) {
+        console.error("Error ", error);
+        await prisma.$disconnect();
+        process.exit(1)
+    }
+}
+main();
