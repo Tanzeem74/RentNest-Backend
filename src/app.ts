469fc3e0cc71b18ea949f686eb52be524f260669
Tanzeem <tanzeemafsar74@gmail.com>
2026-08-03 21:54:21 +0600

    added global error and notfound files

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
 import cookieParser from "cookie-parser";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+import { notFound } from "./middlewares/notFound";
 
 const app: Application = express();
 
@@ -18,5 +20,11 @@ app.use(cookieParser());
 app.get('/', async (req: Request, res: Response) => {
     res.send('Welcome to the RentNest API');
 })
+
+
+
+
+app.use(notFound);
+app.use(globalErrorHandler);
 
 export default app;
